bfd32a92b414dfd91eb870169b5ae590bd9871db
Auguste VN <auguste.vnieuwenhuyzen@gmail.com>
2017-09-25 13:24:09 +0200

    databinding ingesteld

--- CurrencyConverter/app/src/main/java/startup/nmct/currencyconverter/view/MainActivity.java
+++ CurrencyConverter/app/src/main/java/startup/nmct/currencyconverter/view/MainActivity.java
@@ -1,5 +1,6 @@
 package startup.nmct.currencyconverter.view;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,14 +11,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import startup.nmct.currencyconverter.R;
+import startup.nmct.currencyconverter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // setContentView(R.layout.activity_main);
+        // Databinding
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -27,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -50,5 +58,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
